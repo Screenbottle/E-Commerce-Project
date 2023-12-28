d62f36dc1c2f4939077f6b20f5b4f83c0784a55f
Screenbottle <arvid.rydh@gmail.com>
2023-12-28 20:33:09 +0100

    added adress details to customer

--- ECommerce.Data/Entities/Customer.cs
+++ ECommerce.Data/Entities/Customer.cs
@@ -5,11 +5,20 @@ public class Customer : IEntity
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+
+    public string Street {get; set;}
+    public string PostCode {get; set;}
+    public string City {get; set;}
+    public string? Apartment {get; set;}
  
     public ICollection<Order>? Orders { get; set; } = new List<Order>();
 
-    public Customer(string firstName, string lastName) {
+    public Customer(string firstName, string lastName, string street, string postcode, string city, string? apartment) {
         FirstName = firstName;
         LastName = lastName;
+        Street = street;
+        PostCode = postcode;
+        City = city;
+        Apartment = apartment;
     }
 }
